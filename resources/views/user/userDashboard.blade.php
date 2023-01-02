cb9c66f9f41324780606baf1f6ab0aba678df3eb
pranitlama <pranitlama211@gmail.com>
2023-01-02 20:00:43 +0545

    right panel user

--- resources/views/user/userDashboard.blade.php
+++ resources/views/user/userDashboard.blade.php
@@ -52,6 +52,15 @@
           height: 55px;
           padding: 7px;
       }
+      .left-btn{
+        border-bottom-left-radius: 12px;
+        border-top-left-radius: 12px;
+      }
+
+      .right-btn{
+        border-bottom-right-radius: 12px;
+        border-top-right-radius: 12px;
+      }
     </style>
 </head>
 
@@ -143,14 +152,14 @@
       <div class="post-image pt-2">
         <img src="review6.png" class="img-fluid" alt="">
       </div>
-      <div class="post-footer pt-2">
+      <div class="post-footer pt-3 py-4 d-flex align-items-center">
         <div class="btn-group" role="group">
           <button type="button" class="left-btn post-btn bg-secondary-color border-0 text-black p-1">
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-share-fill" viewBox="0 0 16 16">
               <path d="M11 2.5a2.5 2.5 0 1 1 .603 1.628l-6.718 3.12a2.499 2.499 0 0 1 0 1.504l6.718 3.12a2.5 2.5 0 1 1-.488.876l-6.718-3.12a2.5 2.5 0 1 1 0-3.256l6.718-3.12A2.5 2.5 0 0 1 11 2.5z"/>
             </svg>
           </button>
-          <button type="button" class="left-btn post-btn bg-secondary-color border-0 text-black p-1">
+          <button type="button" class="right-btn post-btn bg-secondary-color border-0 text-black p-1">
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-reply-fill" viewBox="0 0 16 16">
               <path d="M5.921 11.9 1.353 8.62a.719.719 0 0 1 0-1.238L5.921 4.1A.716.716 0 0 1 7 4.719V6c1.5 0 6 0 7 8-2.5-4.5-7-4-7-4v1.281c0 .56-.606.898-1.079.62z"/>
             </svg>
@@ -158,6 +167,88 @@
         </div>
       </div>
     </div>
+
+    <div class="post bg-white border-gray mt-4">
+      <div class=" pt-2 d-flex justify-content-between">
+        <div class="d-flex">
+          <img src="review6.png" class="post-profile rounded-circle" alt="">
+          <div class="d-flex-column">
+            <span class="fw-bold fs-6">username</span>
+        
+          </div>
+        </div>
+        <div class="p-2 text-gray-darker">
+          <button class="btn rounded-circle hover-dark p-1">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-x" viewBox="0 0 14 14">
+              <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
+            </svg>
+          </button>
+        </div>
+      </div>
+      <div class="post-body pt-2 ps-3">
+        <div class="post-title fw-bold">
+          <a href="" class="text-decoration-none text-black">
+            this is the title of the post
+          </a>
+        </div>
+        <div class="post-text pt-1">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate iste, repellat delectus voluptatem qui corrupti error et deserunt eius quos laudantium numquam fugit est autem?
+        </div>
+      </div>
+      <div class="post-image pt-2">
+        <img src="review6.png" class="img-fluid" alt="">
+      </div>
+      <div class="post-footer pt-3 py-4 d-flex align-items-center">
+        <div class="btn-group" role="group">
+          <button type="button" class="left-btn post-btn bg-secondary-color border-0 text-black p-1">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-share-fill" viewBox="0 0 16 16">
+              <path d="M11 2.5a2.5 2.5 0 1 1 .603 1.628l-6.718 3.12a2.499 2.499 0 0 1 0 1.504l6.718 3.12a2.5 2.5 0 1 1-.488.876l-6.718-3.12a2.5 2.5 0 1 1 0-3.256l6.718-3.12A2.5 2.5 0 0 1 11 2.5z"/>
+            </svg>
+          </button>
+          <button type="button" class="right-btn post-btn bg-secondary-color border-0 text-black p-1">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-reply-fill" viewBox="0 0 16 16">
+              <path d="M5.921 11.9 1.353 8.62a.719.719 0 0 1 0-1.238L5.921 4.1A.716.716 0 0 1 7 4.719V6c1.5 0 6 0 7 8-2.5-4.5-7-4-7-4v1.281c0 .56-.606.898-1.079.62z"/>
+            </svg>
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <ul class="list-group">
+      <li class="list-group-item d-flex justify-content-between">
+        <div class="ps-2 lh-1">
+          <div class="fw-bold">Group to follow</div>
+        </div>
+      </li>
+      <li class="list-group-item d-flex justify-content-between">
+        <div class="">
+          <img src="review6.png" height="20" width="20"  class="rounded" alt="" srcset="">
+        </div>
+        <div class="ps-2 lh-1">
+          <div class="fw-bold">Confession</div>
+          <div class="text-secondary fw-light"><small>  Lorem ipsum dolor sit amet........</small></div>
+        </div>
+      </li>
+      <li class="list-group-item d-flex justify-content-between">
+        <div class="">
+          <img src="review6.png" height="20" width="20"  class="rounded" alt="" srcset="">
+        </div>
+        <div class="ps-2 lh-1">
+          <div class="fw-bold">Confession</div>
+          <div class="text-secondary fw-light"><small>  Lorem ipsum dolor sit amet........</small></div>
+        </div>
+      </li>
+      <li class="list-group-item d-flex justify-content-between">
+        <div class="">
+          <img src="review6.png" height="20" width="20"  class="rounded" alt="" srcset="">
+        </div>
+        <div class="ps-2 lh-1">
+          <div class="fw-bold">Confession</div>
+          <div class="text-secondary fw-light"><small>  Lorem ipsum dolor sit amet........</small></div>
+        </div>
+      </li>
+    </ul>
   </div>
 </div>
   </div>
