89d55f81bbe64c435c241b6f86dba34086b1664a
pranitlama <pranitlama211@gmail.com>
2023-02-20 19:37:34 +0545

    like and edit post

--- resources/views/user/userDashboard.blade.php
+++ resources/views/user/userDashboard.blade.php
@@ -106,9 +106,9 @@
 
                                         <button class="btn   p-1" >
                                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
-  <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
-  <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
-</svg>
+                                        <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
+                                        <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
+                                        </svg>
                                         </button>
                                         </a>
                                     <a href="{{route('userdeletePost',$post['id'])}}">
@@ -158,8 +158,8 @@
                            
                             <div class="post-footer pt-3 py-4 d-flex align-items-center">
                                 <div class="btn-group ps-2 " role="group">
-                                    <form  id="likeform">
-                                    <input type="hidden" name="postid" id="postid" value="{{$post['id']}}">
+                                    <span  class="likeform">
+                                    <input type="hidden" class="idpost" name="postid" id="postid" value="{{$post['id']}}">
                                     
                                   
                                     <button type="submit"
@@ -176,8 +176,8 @@
                                      
                                                                                                                                                
                                             @if($postlike->count()>0)
-                                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="lightblue" class="bi bi-heart-fill" viewBox="0 0 16 16">
-                                        <path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"/>
+                                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" id="postdis" fill="lightblue"  class="bi bi-heart-fill" viewBox="0 0 16 16">
+                                        <path  d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"/>
                                         </svg> <span id="likecount">{{$postcount}}</span> Like
                                     
                                         @else
@@ -186,7 +186,7 @@
                                       
       
                                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" id="postdis" fill="currentColor" class="bi bi-heart-fill" viewBox="0 0 16 16">
-                                        <path fill-rule="evenodd" d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z"/>
+                                        <path  d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"/>
                                         </svg> <span id="likecount">{{$postcount}}</span> Like
 
                                       
@@ -195,16 +195,16 @@
                                         
 
                                     </button>
-                                    </form>
+                                    </span>
                                  
                                     <button type="button"
-                                        class=" post-btn bg-transparent  border-0 ms-2 text-black p-1">
+                                        class="  bg-transparent  border-0 ms-2 text-black p-1">
                                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chat" viewBox="0 0 16 16">
                                     <path d="M2.678 11.894a1 1 0 0 1 .287.801 10.97 10.97 0 0 1-.398 2c1.395-.323 2.247-.697 2.634-.893a1 1 0 0 1 .71-.074A8.06 8.06 0 0 0 8 14c3.996 0 7-2.807 7-6 0-3.192-3.004-6-7-6S1 4.808 1 8c0 1.468.617 2.83 1.678 3.894zm-.493 3.905a21.682 21.682 0 0 1-.713.129c-.2.032-.352-.176-.273-.362a9.68 9.68 0 0 0 .244-.637l.003-.01c.248-.72.45-1.548.524-2.319C.743 11.37 0 9.76 0 8c0-3.866 3.582-7 8-7s8 3.134 8 7-3.582 7-8 7a9.06 9.06 0 0 1-2.347-.306c-.52.263-1.639.742-3.468 1.105z"/>
                                     </svg> 2 Comments
                                     </button>
                                     <button type="button"
-                                        class="report-btn post-btn bg-transparent  border-0 ms-2 text-black p-1">
+                                        class="report-btn  bg-transparent  border-0 ms-2 text-black p-1">
                                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-flag" viewBox="0 0 16 16">
                                         <path d="M14.778.085A.5.5 0 0 1 15 .5V8a.5.5 0 0 1-.314.464L14.5 8l.186.464-.003.001-.006.003-.023.009a12.435 12.435 0 0 1-.397.15c-.264.095-.631.223-1.047.35-.816.252-1.879.523-2.71.523-.847 0-1.548-.28-2.158-.525l-.028-.01C7.68 8.71 7.14 8.5 6.5 8.5c-.7 0-1.638.23-2.437.477A19.626 19.626 0 0 0 3 9.342V15.5a.5.5 0 0 1-1 0V.5a.5.5 0 0 1 1 0v.282c.226-.079.496-.17.79-.26C4.606.272 5.67 0 6.5 0c.84 0 1.524.277 2.121.519l.043.018C9.286.788 9.828 1 10.5 1c.7 0 1.638-.23 2.437-.477a19.587 19.587 0 0 0 1.349-.476l.019-.007.004-.002h.001M14 1.221c-.22.078-.48.167-.766.255-.81.252-1.872.523-2.734.523-.886 0-1.592-.286-2.203-.534l-.008-.003C7.662 1.21 7.139 1 6.5 1c-.669 0-1.606.229-2.415.478A21.294 21.294 0 0 0 3 1.845v6.433c.22-.078.48-.167.766-.255C4.576 7.77 5.638 7.5 6.5 7.5c.847 0 1.548.28 2.158.525l.028.01C9.32 8.29 9.86 8.5 10.5 8.5c.668 0 1.606-.229 2.415-.478A21.317 21.317 0 0 0 14 7.655V1.222z"/>
                                         </svg>
@@ -213,7 +213,7 @@
                             </div>
                         </div>
                         
-                        @endif
+                       @endif
                         @endforeach
                     </div>
                 </div>
@@ -262,12 +262,12 @@
   <script src="{{asset('assets/js/jquery-3.6.0.min.js')}}"></script>
   <script>
   
-   $('#likeform').submit(function(e)
+   $('.post-btn').click(function(e)
    { 
-    e.preventDefault();
-    var postid=$('#postid').val();
-   
-    // alert(postid);
+    // e.preventDefault();
+    var postid=$(this).closest(".likeform").find('.idpost').val();
+   var self=this;
+    //  alert(postid);
    
 $.ajax({
     type:'post',
@@ -280,16 +280,21 @@ $.ajax({
        {
         
         // $('#likecount').html()={{$postcount}};
-        $('#postdis').attr('fill','lightblue');
-        // alert(response.postcount);
-        $('#likecount').html(response.postcount);
+       $(self).closest(".post-btn").find('#postdis').attr('fill','lightblue');
+    // alert(response.postcount);
+    // $(self).closest(".post-btn").find('#postdis').removeClass('notliked');
+    //    $(self).closest(".post-btn").find('#postdis').addClass('liked');
+        $(self).closest(".post-btn").find('#likecount').html(response.postcount);
         
         
         }
         else if(response.msg=="disliked"){
+            
+            $(self).closest(".post-btn").find('#postdis').attr('fill','black');
+            // $(self).closest(".post-btn").find('#postdis').removeClass('liked');
+            // $(self).closest(".post-btn").find('#postdis').addClass('notliked');
+            $(self).closest(".post-btn").find('#likecount').html(response.postcount);
 
-            $('#postdis').attr('fill','black');
-            $('#likecount').html(response.postcount);
         }
         
 
