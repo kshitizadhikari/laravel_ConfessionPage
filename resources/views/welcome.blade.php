d68bbba1e06f5e39e33517996e29b03f608fd377
pranitlama <pranitlama211@gmail.com>
2023-03-14 10:21:24 +0545

    try

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,6 +10,7 @@
     <!-- <meta content="width=device-width, initial-scale=1.0" name="viewport">
     <meta content="Free HTML Templates" name="keywords">
     <meta content="Free HTML Templates" name="description"> -->
+    <!-- try matra ho -->
 
     <!-- Favicon -->
     <link href="img/favicon.ico" rel="icon">
